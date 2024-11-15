fb4ade110d976cfaef1bb83f5d0a6f5d098f538e
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-11-16 00:02:16 +0700

    Add: Ex2 - Navigation with view

---
+++ NavigationView/EX2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EX2ViewController.swift
+//  Swift_ReviewSeries
+//
+//  Created by Nguyễn Văn Hiếu on 15/11/24.
+//
+
+import UIKit
+
+class EX2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
