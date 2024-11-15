0faf46b576132dd1848aa876d31443eebf45fb9f
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-11-16 00:54:04 +0700

    Add: Ex3 - TableViewController & Custom Cell (p1)

---
+++ Swift_ReviewSeries/Views/Table/Ex3Cell/EX3TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EX3TableViewCell.swift
+//  Swift_ReviewSeries
+//
+//  Created by Nguyễn Văn Hiếu on 16/11/24.
+//
+
+import UIKit
+
+class EX3TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
