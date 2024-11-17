4b6d1c3d8a49db4e5a6c1d0307918bc21b556699
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-11-17 21:19:02 +0700

    Add:  Created a custom cell for the table

---
+++ Swift_ReviewSeries/Views/Table/Ex3/Cell/AutomaticRowHeightTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AutomaticRowHeightTableViewCell.swift
+//  Swift_ReviewSeries
+//
+//  Created by Nguyễn Văn Hiếu on 16/11/24.
+//
+
+import UIKit
+
+class AutomaticRowHeightTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Swift_ReviewSeries/Views/Table/Ex3/Cell/BaseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BaseTableViewCell.swift
+//  Swift_ReviewSeries
+//
+//  Created by Nguyễn Văn Hiếu on 16/11/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
